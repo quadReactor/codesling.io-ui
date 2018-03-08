80cdf9e53408bab93ebf8f007cb4f748d6e51b7a
Elbert Chao <elbertchao@gmail.com>
2018-03-07 19:54:56 -0800

    implmented logout button

--- client/src/components/Sling/EditorHeader/EditorNavbar.js
+++ client/src/components/Sling/EditorHeader/EditorNavbar.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '../../globals/Button';
+// const EditorNavbar = () => (
+//   <nav className="editor-navbar">
+//     <ul>
+//       <li>Item 1</li>
+//       <li>Item 2</li>
+//       <li>Item 3</li>
+//     </ul>
+//   </nav>
+// );
 
-const EditorNavbar = () => (
-  <nav className="editor-navbar">
+class EditorNavbar extends Component {
+  render() {
+    return (
+      <nav className="editor-navbar">
     <ul>
       <li>Item 1</li>
       <li>Item 2</li>
-      <li>Item 3</li>
+      <li><a onClick={() => {window.localStorage.clear(); window.location.reload(); }}> Logout </a></li>
     </ul>
   </nav>
-);
+    )
+  }
+}
 
 export default EditorNavbar;
