2fe35824b164f2d6332eadb1e1e07ab9aa8868c2
Elbert Chao <elbertchao@gmail.com>
2018-03-08 21:30:11 -0800

    started working on friend component

---
+++ client/src/components/Friend/FriendList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const FriendList = ({ friend }) => {
+  return (
+    <div>
+      
+      {/* {friends.map(friend => { */}
+      return (
+      <li>
+        <div>Friend</div>
+      </li>
+      )
+      {/* })
+        } */}
+    </div>
+  );
+};
