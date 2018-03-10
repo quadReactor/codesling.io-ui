0f972a04857fd018fd7f99802844e939cba89351
Elbert Chao <elbertchao@gmail.com>
2018-03-10 11:50:39 -0800

    at chat to friend list, commented approach to impement

--- client/src/components/Friend/index.jsx
+++ client/src/components/Friend/index.jsx
@@ -7,43 +7,61 @@ import { FriendList } from "./FriendList.jsx";
 import io from 'socket.io-client/dist/socket.io.js';
 import Chat from '../Chat/index.jsx'
 import EditorNavbar from "../Sling/EditorHeader";
-
+const { SOCKET_SERVER_URL } = process.env;
+const { REST_SERVER_URL } = process.env;
 class Friend extends Component {
   state = {
     friends: [],
     check: false,
-    socket: null
+    socket: null,
+    room: '',
+    chat: false, //render only chat if you open chat with friend
   };
 
   componentWillMount () {
     //this is the handshake
-    this.socket = io('http://localhost:4155', {
+    this.socket = io(SOCKET_SERVER_URL, {
       query: {
         //roomId cahnge it to what i want must be string removes / with slice 
-        roomId: '1234567890qwertyuiop'
+        roomId: this.state.room
       }
     });
 
     this.setState({ socket: this.socket });
   }
+
+  roomId = (user, friend) => {
+    //when user clicks chat button it sets up room id for socket
+      // create button next for chatting in fron ot friend
+    //takes user user_id and friend user_id
+    //Sort return String
+    //exampke userId = 1 friendId = 2
+    //return 'chatroom12"
+    //set state room to this id
+  }
+
   fetchFriends = async () => {
     const id = localStorage.getItem("id");
     const { data } = await axios.get(
-      `http://localhost:3396/api/friends/fetchAllFriends/${id}`
+      `${REST_SERVER_URL}/api/friends/fetchAllFriends/${id}`
     );
     this.setState({ friends: data });
+
+    //fetch by user and friend id to make sure there arnt repeate combinations of friends
   }
   
   addFriend = async (e) => {
+    //need logic to not add friend only add if not already friend
+    //current logic is like following people
     e.preventDefault()
     const user = this.state.name;
     const { data } = await axios.get(
-      `http://localhost:3396/api/users/fetchSearchUser/${user}`
+      `${REST_SERVER_URL}/api/users/fetchSearchUser/${user}`
     );
     if (!data.rows[0]) {
       await this.setState({ check: true });
     } else {
-      await axios.post(`http://localhost:3396/api/friends/addFriend`, {
+      await axios.post(`${REST_SERVER_URL}/api/friends/addFriend`, {
         user_id: window.localStorage.id,
         friend_id: data.rows[0].id
       });
@@ -58,7 +76,7 @@ class Friend extends Component {
   removeFriend = async (friend) => {
     const userId = localStorage.getItem("id");
     const friendId = friend;
-    const {data} = await axios.delete(`http://localhost:3396/api/friends/deleteFriend/${userId}/${friendId}`);
+    const {data} = await axios.delete(`${REST_SERVER_URL}/api/friends/deleteFriend/${userId}/${friendId}`);
     this.fetchFriends();
   };
 
@@ -97,6 +115,7 @@ class Friend extends Component {
         </div>
         <div>
           <h3>Messages</h3>
+          <h4>hi</h4>
           <Chat socket={this.state.socket} />
         </div>
       </div>
